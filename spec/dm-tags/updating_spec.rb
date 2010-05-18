4569d6548258c706636bd95ff2dd9a29c3bac6d4
Dan Kubb <dan.kubb@gmail.com>
2010-05-18 13:33:55 -0700

    Added guard clauses to prevent specs being run with in_memory and yaml
    * dm-tags relies on many to many relationships, which the in_memory and
      yaml adapters do not support yet.

--- spec/dm-tags/updating_spec.rb
+++ spec/dm-tags/updating_spec.rb
@@ -1,44 +1,46 @@
 require 'spec_helper'
 
 describe "Tag", "when updating" do
-  before do
-    @tagged_model = TaggedModel.new
-  end
-
-  it "should create itself" do
-    @tagged_model.tag_list = "abc, def, ghi"
-    @tagged_model.skill_list = "Casablanca, Morocco"
-    @tagged_model.save.should be_true
-    @tagged_model.should be_saved
-
-    @tagged_model.reload
-    @tagged_model.tags.map { |t| t.name }.should == %w[ abc def ghi ]
-    @tagged_model.skills.map { |t| t.name }.should == %w[ Casablanca Morocco ]
-  end
-
-  it "should update itself" do
-    @tagged_model.save.should be_true
-    @tagged_model.should be_saved
-
-    @tagged_model.reload
-    @tagged_model.tags.should be_empty
-    @tagged_model.skills.should be_empty
-
-    @tagged_model.tag_list = "abc, def, xyz, jkl"
-    @tagged_model.skill_list = "Sahara, Morocco"
-    @tagged_model.save.should be_true
-
-    @tagged_model.reload
-    pending do
-      @tagged_model.tags.map { |t| t.name }.should == %w[ abc def jkl xyz ]
+  supported_by :sqlite, :mysql, :postgres do
+    before do
+      @tagged_model = TaggedModel.new
     end
-    @tagged_model.skills.map { |t| t.name }.should == %w[ Morocco Sahara ]
 
-    @tagged_model.tag_list = ""
-    @tagged_model.skill_list = ""
-    @tagged_model.save
+    it "should create itself" do
+      @tagged_model.tag_list = "abc, def, ghi"
+      @tagged_model.skill_list = "Casablanca, Morocco"
+      @tagged_model.save.should be_true
+      @tagged_model.should be_saved
 
-    @tagged_model.tags.should == []
-    @tagged_model.skills.should == []
+      @tagged_model.reload
+      @tagged_model.tags.map { |t| t.name }.should == %w[ abc def ghi ]
+      @tagged_model.skills.map { |t| t.name }.should == %w[ Casablanca Morocco ]
+    end
+
+    it "should update itself" do
+      @tagged_model.save.should be_true
+      @tagged_model.should be_saved
+
+      @tagged_model.reload
+      @tagged_model.tags.should be_empty
+      @tagged_model.skills.should be_empty
+
+      @tagged_model.tag_list = "abc, def, xyz, jkl"
+      @tagged_model.skill_list = "Sahara, Morocco"
+      @tagged_model.save.should be_true
+
+      @tagged_model.reload
+      pending do
+        @tagged_model.tags.map { |t| t.name }.should == %w[ abc def jkl xyz ]
+      end
+      @tagged_model.skills.map { |t| t.name }.should == %w[ Morocco Sahara ]
+
+      @tagged_model.tag_list = ""
+      @tagged_model.skill_list = ""
+      @tagged_model.save
+
+      @tagged_model.tags.should == []
+      @tagged_model.skills.should == []
+    end
   end
 end
